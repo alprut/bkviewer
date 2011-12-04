0429b66015e5c23641bf6c5ce804a44154c59c43
Yasuyuki KOZAKAI <yasuyuki.kozakai@gmail.com>
2011-12-05 01:17:40 +0900

    Introduces a new formatter 'index'

--- content/bkviewer.js
+++ content/bkviewer.js
@@ -113,6 +113,67 @@ return this.each(function() {
 })}} (jQuery));
 
 (function($) {
+$.fn.bkviewer_index = function(options) {
+return this.each(function() {
+	var t = $(this);
+
+	function add_box(target, cur_box, item) {
+		var box;
+
+		if (cur_box.height() > 500) {
+			box = $('<ul />').addClass('bk-box')
+					 .appendTo(target);
+
+			box.append(item);
+		} else {
+			box = cur_box;
+		}
+
+		return box;
+	}
+
+	t.show_bookmarks({
+		init: function(target, context) {
+			context['cur_box'] = $('<ul />').addClass('bk-box')
+							.appendTo(target);
+		},
+		add_category_view: function(json, target, context) {
+			var box, cur_box = context['cur_box'];
+
+			if (json['type'] != "text/x-moz-place-container")
+				return null;
+
+
+			box = $('<li />').html(json['title'])
+					 .addClass('bk-category')
+					 .appendTo(cur_box);
+
+			context['cur_box'] = add_box(target, cur_box, box);
+		},
+
+		add_item_view: function(json, category_view, target, context) {
+			var box, cur_box = context['cur_box'];
+
+			if (json['type'] != "text/x-moz-place")
+				return;
+
+			box = $('<li />').addClass('bk-item')
+					 .appendTo(cur_box);
+
+			$('<a />').html(json['title'])
+				  .attr({'href': json['uri']})
+				  .addClass('bk-item')
+				  .appendTo(box);
+
+			context['cur_box'] = add_box(target, cur_box, box);
+		}
+	});
+
+	$('.bk-box').css({"height": 500});
+
+})}} (jQuery));
+
+(function($) {
 $.fn.equal_spacing = function(options) {
 return this.each(function() {
 
