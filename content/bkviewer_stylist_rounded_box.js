d9acb950326757c1890c58d64837cf0f54d048ea
Yasuyuki KOZAKAI <yasuyuki.kozakai@gmail.com>
2012-01-21 20:22:59 +0900

    Applies the color configuration to rounded_box theme

--- content/bkviewer_stylist_rounded_box.js
+++ content/bkviewer_stylist_rounded_box.js
@@ -1,5 +1,18 @@
 (function($) {
-	var json = {
+	var json;
+	var prefs = nsPreferences;
+	var key_base = "extensions.bkviewer.";
+	var frame_color = prefs.copyUnicharPref(key_base + "frame_color",
+						"#0ac");
+	var bg_color =    prefs.copyUnicharPref(key_base + "bg_color",
+						"#fff");
+	var text_color =  prefs.copyUnicharPref(key_base + "text_color",
+						"#00f");
+	json = {
+		"body": {
+			"background-color": bg_color,
+			"color": text_color,
+		},
 		".es-clearfix:after": {
 			"content":    "\".\"",
 			"clear":      "both",
@@ -26,15 +39,15 @@
 			"padding-right": "0pt",
 			"margin-left":   "0pt",
 			"margin-right":  "0pt",
-			"border-color":  "#0ac",
+			"border-color":  frame_color,
 			"border-width":  "1pt",
 			"border-style":  "solid",
 			"border-radius": "10pt",
 		},
 		
 		"li.bk-category": {
-			"color":	    "white",
-			"background-color": "#0ac",
+			"color":	    bg_color,
+			"background-color": frame_color,
 			"text-align":       "center",
 			"font-weight":      "bold",
 			"list-style-type":  "none",
@@ -62,6 +75,7 @@
 		
 		"a.bk-item": {
 			"text-decoration": "none",
+			"color": text_color,
 		},
 		
 		"img.favicon": {
