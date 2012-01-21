2d997916c60a857c0fca59da7557ba627d5319f9
Yasuyuki KOZAKAI <yasuyuki.kozakai@gmail.com>
2012-01-21 17:18:38 +0900

    Improves the white spaces in tclock.js

--- content/tclock.js
+++ content/tclock.js
@@ -7,56 +7,52 @@
  * Free to use under the BSD license.
  * ========================================================== */  
 (function($) {
-	$.fn.tclock = function(options) {
-		return this.each(function(){
-			var target = $(this);
-			var targetTime = target.find('.tclock-time');
-			var targetDate = target.find('.tclock-date');
+$.fn.tclock = function(options) {
+return this.each(function(){
+	var target = $(this);
+	var targetTime = target.find('.tclock-time');
+	var targetDate = target.find('.tclock-date');
 
-			var ticker = {
-				start : function() {
-					ticker.update();
-					setInterval(ticker.update, 1000);
-				},
-				update : function() {
-					function twoDigits(num) {
-						return String("00" + num)
-								.slice(-2);
-					}
-
-					var now     = new Date();
-					var year    = now.getFullYear();
-					var month   = now.getMonth();
-					var date    = now.getDate();
-					var day     = now.getDay();
-					var hours   = now.getHours();
-					var minutes = now.getMinutes();
-					var seconds = now.getSeconds();
-
-					var month_str = new Array(
-						"Jan", "Feb", "Mar", "Apr",
-						"May", "Jun", "Jul", "Aug",
-						"Sep", "Oct", "Nov", "Dec");
-					var week_str = new Array(
-						"Sun", "Mon", "Tue", "Wed",
-						"Thu", "Fri", "Sat");
-
-					var time_str
-						= twoDigits(hours) + ":" +
-						  twoDigits(minutes) + ":" +
-						  twoDigits(seconds);
-					var date_str
-						= week_str[day] + " " +
-						  month_str[month] + " " +
-						  twoDigits(date) + ", " +
-						  year;
-
-					targetTime.html(time_str);
-					targetDate.html(date_str);
-				}
+	var ticker = {
+		start : function() {
+			ticker.update();
+			setInterval(ticker.update, 1000);
+		},
+		update : function() {
+			function twoDigits(num) {
+				return String("00" + num)
+						.slice(-2);
 			}
 
-			ticker.start();
-		})
+			var now     = new Date();
+			var year    = now.getFullYear();
+			var month   = now.getMonth();
+			var date    = now.getDate();
+			var day     = now.getDay();
+			var hours   = now.getHours();
+			var minutes = now.getMinutes();
+			var seconds = now.getSeconds();
+
+			var month_str = new Array(
+				"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+				"Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+			var week_str = new Array(
+				"Sun", "Mon", "Tue", "Wed",
+				"Thu", "Fri", "Sat");
+
+			var time_str = twoDigits(hours) + ":" +
+				       twoDigits(minutes) + ":" +
+				       twoDigits(seconds);
+
+			var date_str = week_str[day] + " " +
+				       month_str[month] + " " +
+				       twoDigits(date) + ", " +
+				       year;
+
+			targetTime.html(time_str);
+			targetDate.html(date_str);
+		}
 	}
-}(jQuery));
+
+	ticker.start();
+})}}(jQuery));
