79fc1d9f8123b4d431c2cda7732b626d04a50884
Yasuyuki Kozakai <yasuyuki.kozakai@gmail.com>
2012-12-28 18:02:55 +0900

    Makes the perferene remember the hidden categories

--- content/bkviewer_rounded_box.js
+++ content/bkviewer_rounded_box.js
@@ -2,6 +2,36 @@
 $.fn.bkviewer_rounded_box = function(options) {
 return this.each(function() {
 	var t = $(this);
+	var hiddens = new Object();
+
+	function remove_item(hiddens, i) {
+		var items = hiddens.items, head, tail;
+
+		head = items.slice(0, i);
+		tail = items.slice(i+1, items.length);
+		hiddens.items = head.concat(tail);
+	}
+
+	function toggle_and_save(box, title, hiddens) {
+		var prefs = nsPreferences;
+		var key = "extensions.bkviewer.hiddens";
+
+		box.slideToggle(function() {
+			var box = $(this);
+			var i = hiddens.items.indexOf(title);
+
+			if (box.css('display') == 'none') {
+				if (i == -1)
+					hiddens.items.push(title);
+			} else {
+				if (i != -1)
+					remove_item(hiddens, i);
+			}
+
+			prefs.setUnicharPref(key,
+					     JSON.stringify(hiddens.items));
+		});
+	}
 
 	t.show_bookmarks({
 		add_category_view: function(json, target, context) {
@@ -14,7 +44,9 @@ return this.each(function() {
 				   .addClass('bk-category')
 				   .appendTo(box)
 				   .click(function() {
-					$(this).next().slideToggle();
+					toggle_and_save($(this).next(),
+							json['title'],
+							hiddens);
 				   });
 
 			box = $('<ul />').addClass('bk-item')
@@ -52,6 +84,15 @@ return this.each(function() {
 
 		equal_spacing(t, item);
 	});
+
+	get_hiddens(hiddens);
+
+	function get_hiddens(hiddens) {
+		var prefs = nsPreferences;
+		var key = "extensions.bkviewer.hiddens";
+
+		hiddens.items = JSON.parse(prefs.copyUnicharPref(key, "[]"));
+	}
 
 	function equal_spacing(box, contents) {
 		var box_width, item_width, items_num, margin;
